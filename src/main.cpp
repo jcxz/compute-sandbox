e671530493cff17b3588b7b5b9ca49ec7d94d4bf
Matus Fedorko <mat.fedorko@gmail.com>
2026-03-27 22:18:49 +0100

    Add command line options for GPU initialization flags

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,30 @@
 #include "gpu/gpu.h"
 #include <iostream>
+#include <string_view>
 
 extern bool testInvert();
 
 
-int main()
+int main(const int argc, char* argv[])
 {
-	const uint32_t flags = GpuFlags::Debug | GpuFlags::RDocCapture | GpuFlags::PreloadKernels;
+	uint32_t flags = GpuFlags::Debug | GpuFlags::RDocCapture | GpuFlags::PreloadKernels;
+	for (int i = 1; i < argc; ++i)
+	{
+		const std::string_view arg = argv[i];
+		if (arg == "--enable-debug")
+		{
+			flags |= GpuFlags::Debug;
+		}
+		else if (arg == "--disable-capture")
+		{
+			flags &= ~GpuFlags::RDocCapture;
+		}
+		else if (arg == "--disable-preload")
+		{
+			flags &= ~GpuFlags::PreloadKernels;
+		}
+	}
+
 	if (!InitializeGpu(flags))
 	{
 		std::cerr << "Failed to initialize GPU" << std::endl;
